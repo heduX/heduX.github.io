64c9b73ca77699184c942b6702131340a85a3306
heduX <161673066+heduX@users.noreply.github.com>
2024-03-23 22:49:24 +0800

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,7 @@
+document.getElementById("icon-header").onclick = function () {
+  document.getElementById("bg-menu").style.display = "block";
+  document.getElementById(icon - header).style.display = "none";
+};
+document.getElementById("bg-menu").onclick = function () {
+  document.getElementById("bg-menu").style.display = "none";
+};
