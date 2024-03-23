ac5adaceeca072c6484a37064d254f7afb78dab1
heduX <161673066+heduX@users.noreply.github.com>
2024-03-23 22:56:44 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -3,4 +3,5 @@ document.getElementById("icon-header").onclick = function () {
   document.getElementById(icon - header).style.display = "none";
 };
 document.getElementById("bg-menu").onclick = function () {
-  document.getElementById("bg-menu").style.disp
+  document.getElementById("bg-menu").style.display = "none";
+};
